2eeffaec5a16aab870983a5e5b6243f05c655f32
madhur.kumar <madhur.kumar@outlook.com>
2021-01-09 23:53:05 -0500

    AE-11

---
+++ src/main/java/bug/free/broccoli/algoexpert/Ae11.java
@@ -0,0 +1,24 @@
+package bug.free.broccoli.algoexpert;
+
+import java.util.Arrays;
+
+public class Ae11 {
+
+    public static int[] bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {8, 5, 2, 9, 5, 6, 3};
+        Arrays.stream(bubbleSort(input)).forEach(System.out::println);
+    }
+}

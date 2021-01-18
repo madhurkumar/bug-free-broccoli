c0c952ff25ed1d1ad5341aecb25da8474211df6f
madhur.kumar <madhur.kumar@outlook.com>
2021-01-17 21:23:50 -0500

    AeH4-Partial

--- src/main/java/bug/free/broccoli/algoexpert/AeH4.java
+++ src/main/java/bug/free/broccoli/algoexpert/AeH4.java
@@ -5,17 +5,37 @@ import java.util.stream.Collectors;
 
 public class AeH4 {
     public static int minRewards(int[] scores) {
-        int max = 0;
-        int[] result = new int[scores.length - 1];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = scores[i + 1] - scores[i];
+        System.out.println("==>" + Arrays.stream(scores).boxed().map(String::valueOf).collect(Collectors.joining(",")));
+        int index = 0;
+        int sum = 0;
+        boolean asc = scores[0] <= scores[1];
+        for (int i = 1; i <= scores.length; i++) {
+            boolean upward = ((i == scores.length) || (scores[i - 1] > scores[i])) && asc;
+            if (upward) {
+                int counter = index == 0 ? 1 : 2;
+                for (int j = index; j < i; j++) {
+                    sum += scores[j] = counter++;
+                }
+                asc = false;
+                index = i;
+            }
+            boolean downward = ((i == scores.length) || (scores[i - 1] < scores[i])) && !asc;
+            if (downward) {
+                int counter = i - index;
+                for (int j = index; j < i; j++) {
+                    sum += scores[j] = counter--;
+                }
+                asc = true;
+                index = i;
+            }
         }
-        System.out.println(Arrays.stream(result).mapToObj(String::valueOf).collect(Collectors.joining(",")));
-        //return Arrays.stream(result).map(l -> l + m).sum();
-        return -1;
+        System.out.println("==>" + Arrays.stream(scores).boxed().map(String::valueOf).collect(Collectors.joining(",")));
+        return sum;
     }
+
 
     public static void main(String[] args) {
         System.out.println(minRewards(new int[]{8, 4, 2, 1, 3, 6, 7, 9, 5}));
+        System.out.println(minRewards(new int[]{0, 4, 2, 1, 3}));
     }
 }

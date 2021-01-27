9644ca8702bc0c50040439fe71158dbd597b7fb1
madhur.kumar <madhur.kumar@outlook.com>
2021-01-26 23:06:04 -0500

    AeVH2-Initial

--- src/main/java/bug/free/broccoli/algoexpert/AeVH2.java
+++ src/main/java/bug/free/broccoli/algoexpert/AeVH2.java
@@ -1,47 +1,53 @@
 package bug.free.broccoli.algoexpert;
 
-import lombok.ToString;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class AeVH2 {
-
-
-    public static BinaryTree flattenBinaryTree(BinaryTree root) {
-        List<BinaryTree> nodes = new ArrayList<>();
-        traverse(root, nodes);
-        return nodes.get(0);
+    public static List<StringMeeting> calendarMatching(
+            List<StringMeeting> calendar1,
+            StringMeeting dailyBounds1,
+            List<StringMeeting> calendar2,
+            StringMeeting dailyBounds2,
+            int meetingDuration) {
+        parseDailyBound(dailyBounds1, dailyBounds2, meetingDuration);
+        return new ArrayList<>();
     }
 
-    public static void traverse(BinaryTree root, List<BinaryTree> acc) {
-        if (root == null) return;
-        if (root.left != null) traverse(root.left, acc);
-        acc.add(root);
-        if (root.right != null) traverse(root.right, acc);
+    private static Integer[] parseDailyBound(StringMeeting dailyBounds1, StringMeeting dailyBounds2, int meetingDuration) {
+        Integer[] start1 = Arrays.stream(dailyBounds1.start.split(":")).map(Integer::parseInt).toArray(Integer[]::new);
+        Integer[] end1 = Arrays.stream(dailyBounds1.end.split(":")).map(Integer::parseInt).toArray(Integer[]::new);
+        Integer[] start2 = Arrays.stream(dailyBounds2.start.split(":")).map(Integer::parseInt).toArray(Integer[]::new);
+        Integer[] end2 = Arrays.stream(dailyBounds2.end.split(":")).map(Integer::parseInt).toArray(Integer[]::new);
+        int startHour = Math.max(start1[0], start2[0]);
+        int startMinute = Math.max(start1[1], start2[1]);
+        int endHour = Math.min(end1[0], end2[0]);
+        int endMinute = Math.min(end1[1], end2[1]);
+        int noOfMeetingSlots = ((endHour - startHour) * 60) / meetingDuration + (endMinute - startMinute) / meetingDuration;
+        return IntStream
+                .rangeClosed(0, noOfMeetingSlots)
+                .map(i -> startHour * 100 + 100 * ((meetingDuration * i) / 60) + (meetingDuration * i) % 60)
+                .boxed()
+                .toArray(Integer[]::new);
+    }
+
+    static class StringMeeting {
+        public String start;
+        public String end;
+
+        public StringMeeting(String start, String end) {
+            this.start = start;
+            this.end = end;
+        }
     }
 
     public static void main(String[] args) {
-        BinaryTree root = new BinaryTree(1);
-        root.left = new BinaryTree(2);
-        root.right = new BinaryTree(3);
-        root.left.left = new BinaryTree(4);
-        root.left.right = new BinaryTree(5);
-        root.left.right.left = new BinaryTree(7);
-        root.left.right.right = new BinaryTree(8);
-        root.right.left = new BinaryTree(6);
-        System.out.println(flattenBinaryTree(root));
+        calendarMatching(
+                null, new StringMeeting("9:00", "20:00"),
+                null, new StringMeeting("10:00", "18:00"),
+                30);
     }
 
-    // This is the class of the input root. Do not edit it.
-    @ToString
-    static class BinaryTree {
-        int value;
-        BinaryTree left = null;
-        BinaryTree right = null;
-
-        public BinaryTree(int value) {
-            this.value = value;
-        }
-    }
 }
